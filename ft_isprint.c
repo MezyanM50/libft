86b8f933dec686aa6bb4f79b8024323d87016d9a
MezyanM50 <mezyanm50@gmail.com>
2023-10-19 17:09:07 +0100

    add header file (libft.h) and add the header on all the files and ft_memset.c done

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,3 +1,5 @@
+#include "libft.h"
+
 int	ft_isprint(int c)
 {
 	if ((c >= 32 && c <= 126) || (c >= 9 && c <= 13))
---
+++ libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+#endif
